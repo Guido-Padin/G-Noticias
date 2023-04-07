bf2dbaec21fda0af8a6b851b4225645724f81e4e
Guido-Padin <guidopadin17@gmail.com>
2023-04-07 18:02:45 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//------Requires------        
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//------Levantando el Servidor------
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => console.log("Server start in http://localhost:"+app.get("port")));
+
+app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
