78f59090c59b31beead6b96af95faf90602e89d0
Guido-Padin <guidopadin17@gmail.com>
2023-05-23 21:31:46 -0300

    Home terminado

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => console.log("Server start in http://localhost:"+app.get("port")));
 
+app.use(express.static(path.resolve(__dirname,"./public")));
+
 app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/index.html")));
